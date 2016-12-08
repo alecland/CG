7971181c1bed31e70d45e4e2235a468a255df064
alecland <alexisdev@outlook.com>
2016-12-08 14:41:55 +0100

    Update PowerOfThor.cpp
    Change conditions from a-b>0 to a>b
    Total char: 203 --> 195

--- CodeGolf/PowerOfThor.cpp
+++ CodeGolf/PowerOfThor.cpp
@@ -1,2 +1,2 @@
 #include <iostream>
-main(){int X,Y,x,y;std::cin>>X>>Y>>x>>y;std::string r;for(;1;r=""){if(Y-y<0){r="N";y--;}else if(Y-y>0){r="S";y++;}if(X-x<0){x--;r+="W";}else if(X-x>0){x++;r+="E";}std::cout<<r+"\n";}}
+main(){int X,Y,x,y;std::cin>>X>>Y>>x>>y;std::string r;for(;1;r=""){if(y>Y){r="N";y--;}else if(Y>y){r="S";y++;}if(x>X){x--;r+="W";}else if(X>x){x++;r+="E";}std::cout<<r+"\n";}}

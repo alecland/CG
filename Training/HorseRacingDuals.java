e788ad0807911be089b3a4b15652a20d71d82d38
alecland <alexisdev@outlook.com>
2016-12-08 17:36:49 +0100

    Update HorseRacingDuals.java
    Problem solved in java

--- Training/HorseRacingDuals.java
+++ Training/HorseRacingDuals.java
@@ -10,14 +10,23 @@ class Solution {
 
     public static void main(String args[]) {
         Scanner in = new Scanner(System.in);
+        List<Integer> horses = new ArrayList<Integer>();
+        int result = 10000000;
         int N = in.nextInt();
         for (int i = 0; i < N; i++) {
             int pi = in.nextInt();
+            horses.add(pi);
+        }
+        
+        Collections.sort(horses);
+        for (int i = 1; i < horses.size(); i++) {
+            if (Math.abs(horses.get(i) - horses.get(i - 1)) < result)
+                result = Math.abs(horses.get(i) - horses.get(i - 1));
         }
 
         // Write an action using System.out.println()
         // To debug: System.err.println("Debug messages...");
 
-        System.out.println("answer");
+        System.out.println(result);
     }
 }

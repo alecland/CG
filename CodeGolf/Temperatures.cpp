3fe044efbef2453a9d1020359bef1621d0e1bb87
alecland <alexisdev@outlook.com>
2016-12-09 14:40:00 +0100

    Update Temperatures.cpp
    Use ternary operator instead of if statement
    Size: 120 --> 119

--- CodeGolf/Temperatures.cpp
+++ CodeGolf/Temperatures.cpp
@@ -1,2 +1,2 @@
 #include<iostream>
-main(){int N,t,r=999,a;for(std::cin>>N;N--;){std::cin>>t;if(t*t<r*r||t==-r&&a<0)a=r=t;}std::cout<<a;}
+main(){int N,t,r=999,a;for(std::cin>>N;N--;){std::cin>>t;t*t<r*r||t==-r&&a<0?a=r=t:a;}std::cout<<a;}
